1c85ff09fa256ea4cd53e86359d22393e1ebd259
ddubbu <hello.ddubbu@gmail.com>
2021-02-03 14:08:24 +0900

    Complete Intro page with Advertisement list
    특이사항
    1. Reder arr using map method
    2. Use margin: 0 auto to center

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,29 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react'; //, { useState } 
 import '../styles/intro.css';
 
 const Intro = () => {
+  const initAd = [
+    { 
+      title: 'Thanks for using ETON.',
+      content: 'You’re all logged out. So now what?',
+      style: {
+        background: 'white'
+      }
+    },
+    {
+      title: 'Using a Shared Computer?',
+      content: `We've saved some of your preferences for the next time you log in, but if you'd like you can clear those now.`,
+      style: {
+        background: '#FDFAE5'
+      }
+    },
+    {
+      title: 'Do you have the apps?',
+      // img: '../public/images/logo192.png',
+      content : `We’ve got apps for iPhone, iPad, Android phones, tablets, and watches. You probably have one of those.`,
+      style: {
+        background: '#BCD9EA'
+      }
+    }, {
+      title: 'Follow us',
+      content: `…on the Trello Blog, Twitter, and Facebook.
+      We post all kinds of tips and updates, but not an annoying amount`,
+      style: {
+        background: 'white'
+      }
+    }, {
+      title: `Share Trello to get free Trello Gold.`,
+      content: `That’s right. For every member you get to sign up, you’ll get a free month of Trello Gold, up to 12 months. With Trello Gold you get three Power-Ups per board, custom backgrounds, stickers and emoji, 250MB attachments, and saved searches.
+
+      Help us spread the word and get some free Gold.`,
+      style: {
+        background: '#F5EA92'
+      }
+    }
+  ];
 
   return (
-    <div className = "IntroContainer">
-
-      <div className = "Intro">
-        <div className="advertisement">
-          광고1
-        </div>
-        <div className="advertisement">
-          광고2
-        </div>
-        <div className="advertisement">
-          광고3
-        </div>
-        <div className="advertisement">
-          광고4
-        </div>
-        <div className="advertisement">
-          광고5
-        </div>
-      </div>
-    </div>
+    <section className="ad-container">
+      {
+        initAd.map((ele, idx) =>{
+          return (
+            <article className="ad-item" key={idx} style={ele.style}> 
+              { ele.img ? <img src={ ele.img } className="ad-img" ></img> : '' }
+              <h1 className="ad-title"> {ele.title} </h1>
+              <p className="ad-content"> {ele.content} </p>
+            </article>
+          )
+        })
+      }
+    </section>
   )
 }
 
