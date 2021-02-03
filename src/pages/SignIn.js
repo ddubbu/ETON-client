d2e0cd89d9949502c2fc48fb893235687f8a008b
ddubbu <hello.ddubbu@gmail.com>
2021-02-03 17:14:50 +0900

    Solve [#46] Save onchange input

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/signin.css';
 import Logo from '../components/common/Logo.js'
 const SignIn = () => {
 
+  const [ input, setInput ] = useState({
+    email : '',
+    password: ''
+  });
+
+  const inputHandler = (e)=>{
+    setInput({
+      ... input,
+      [e.target.type] : e.target.value
+    })
+  }
+
+  const signInHandler = (e)=>{
+    console.log(input);
+  }
 
   return (
     <section className="signin-wrapper">
       <Logo/>
       <section className="signin-form">
         <p>Log in to ETON</p>
-        <input placeholder="Enter email"/>
-        <input placeholder="Enter password"/>
-        <button> Sign In </button>
+        <input type="email" onChange={inputHandler} placeholder="Enter email"/>
+        <input type="password" onChange={inputHandler} placeholder="Enter password"/>
+        <button onClick={signInHandler}> Sign In </button>
         <p>or</p>
         <button>Continue with github 🤖</button>
         <a href="https://www.naver.com">Sign up for an account</a>
