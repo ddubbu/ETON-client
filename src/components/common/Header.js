2500723da9f7dace233e398df2b320c90d17503a
ddubbu <hello.ddubbu@gmail.com>
2021-02-04 22:18:56 +0900

    [rule] 띄어쓰기 2칸

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
-
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import Logo from "./Logo.js";
 
 const Header = () => {
+  const history = useHistory();
 
-    const history = useHistory();
-
-    return(
-        <div className = "Header">
-            <div>
-                <Link to="/">
-                    <img src="./images/logo-ETON-1080.png" className = "logoimg"/>
-                </Link>
-            </div>
-            <div>
-                여백. 뭐 넣을까?
-            </div>
-            <div>
-                <button onClick={() => {history.push('/users/signup')}}>
-                    Sign Up
-                </button>
-                <button onClick={() => {history.push('/users/signin')}}>
-                    Log In
-                </button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="Header">
+      <div>
+        {/* <Logo /> */}
+      </div>
+      <div>여백. 뭐 넣을까?</div>
+      <div>
+        <button
+          onClick={() => {
+            history.push("/users/signup");
+          }}
+        >
+          Sign Up
+        </button>
+        <button
+          onClick={() => {
+            history.push("/users/signin");
+          }}
+        >
+          Log In
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
